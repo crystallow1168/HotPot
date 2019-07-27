602e7ddec9aae78c578d932a2cbb54e3a00931bc
Crystal.L <crystal.l0815@gmail.com>
2019-07-26 18:05:35 -0700

    Change port for Heroku and local use

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 5000;
+const port = proces.env.PORT || 5000;
 
 app.use('/', express.static('client/dist'))
 app.use(cors());
